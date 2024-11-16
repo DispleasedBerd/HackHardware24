4b1ba0a14ab19f58784a824d4b518a8ab362dc46
Yallen1011 <allensundae11@gmail.com>
2024-11-16 12:20:40 -0500

    Note into class

--- score.py
+++ score.py
@@ -12,3 +12,15 @@ def update_score():
     score_text = font.render(f"Score: {score}", True, SCORE_COLOR)
     track.screen.blit(score_text, (20,20))
 
+
+def is_in_score_zone(note):
+    if track.hit_zone_y - note.note_radius <= note.note_y <= track.hit_zone_y + track.hit_zone_height:
+        return True
+    else:
+        return False
+    
+def calculate_score(note):
+    max_score = 100
+    distance = abs(note.note_y-track.hit_zone_y)
+    score = max_score*2.7**(-distance)
+    
