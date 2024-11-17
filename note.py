921226b6e72ef6fdc5de38f84e881a5518fcd90b
Alyssa Lee <alyssalee0329@gmail.com>
2024-11-16 20:29:22 -0500

    implemented rhythm to create pattern

--- note.py
+++ note.py
@@ -1,100 +1,70 @@
 import pygame
-import sys
-import numpy as np
-import track
-from track import Track
 
-#note properties
+# Constants
 NOTE_COLOR = (255, 255, 255)
-note_radius = 20
-# note_x = track.SCREEN_WIDTH // 2
-# note_y = 0  # Start at the top of the screen
-note_speed = 200  # Pixels per second
-track_x = {"LEFT":185,"DOWN":295,"UP":405,"RIGHT":515}
-note_x = []
-note_y = []
-note_radii = []
-note_track = []
-num_notes = 2
-timing = 50
+NOTE_RADIUS = 20
+NOTE_SPEED = 200  # Pixels per second
 
-def note_exists(i):
-    if note_x[i] != None:
-        return True
-
-def build_notes(tracks):
-    for i in range(num_notes):
-        t = np.random.randint(4)
-        note_x.append(tracks[t].x + 50)
-        note_y.append(0-i*timing)
-        note_radii.append(note_radius)
-        note_track.append(tracks[t].pos)
-
-def get_note_track(i):
-    if note_exists(i):
-        return note_track[i]
-
-def update_note_position(i, dt):
-    if note_exists(i):
-        note_y[i] += note_speed * dt
-
-def draw_note(i, screen):
-    if note_exists(i):
-        pygame.draw.circle(screen, NOTE_COLOR, (int(note_x[i]), int(note_y[i])), note_radii[i])
-
-def reset_note(i):
-    note_x[i] = None
-    note_y[i] = None
-    note_radii[i] = None
-    # note_y[i] = -note_radii[i]
-
-def get_y(i):
-    if note_exists(i):
-        return note_y[i]
-
-def is_in_score_zone(i):
-    if note_exists(i):
-        if track.hit_zone_y - note_radius <= note_y[i] <= track.hit_zone_y + track.hit_zone_height:
-            return True
-        else:
-            return False
-
-def respawn_note(i):
-    if note_exists(i):
-        note_y[i] = 0
-        
-def missed_note(i):
-    if note_exists(i):
-        return get_y(i) > track.SCREEN_HEIGHT + note_radii[i]
-    else:
-        False
 class Note:
-    def __init__(self, Track): # include track here for the track it is on
-        self.NOTE_COLOR = NOTE_COLOR
-        self.note_radius = note_radius
-        self.note_x = Track.x + 50 # should be changed to center of the track it is initialized on
-        self.note_y = note_y
-        self.note_speed = note_speed
-        self.track = track #left, right, up, down
-        print(Track.x)
+    def __init__(self, track, time, note_speed=NOTE_SPEED):
+        """
+        Initialize a Note object.
+        :param track: Track object where the note belongs.
+        :param time: Spawn time (in seconds) for the note.
+        :param note_speed: Speed of the note falling.
+        """
+        self.x = track.x + track.width // 2  # Center of the track
+        self.y = -50  # Start above the screen
+        self.radius = NOTE_RADIUS
+        self.speed = note_speed
+        self.time = time  # Time when the note should appear
+        self.active = True  # Whether the note is active
+        self.color = NOTE_COLOR
+        self.track_index = track.pos
 
-    
+    def update_position(self, dt):
+        """Update the position of the note."""
+        if self.active:
+            self.y += self.speed * dt
 
-    
+    def draw(self, screen):
+        """Draw the note on the screen."""
+        if self.active:
+            pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
 
+    def is_in_score_zone(self, hit_zone_y, hit_zone_height):
+        """Check if the note is in the hit zone."""
+        return hit_zone_y - self.radius <= self.y <= hit_zone_y + hit_zone_height
 
+    def missed(self, screen_height):
+        """Check if the note has moved off the screen."""
+        return self.y > screen_height
 
-    
-            
-    def calculate_score(self):
+    def calculate_score(self, hit_zone_y):
+        """Calculate the score based on the distance from the hit zone."""
         max_score = 100
-        distance = abs(self.note_y-track.hit_zone_y)
-        score = max_score*2.7**(-distance)
+        distance = abs(self.y - hit_zone_y)
+        return max_score * 2.7 ** (-distance)
+    
+def generate_notes_beatmap(beatmap_data, tracks):
+    """
+    Generate Note objects from the beatmap data.
+    :param beatmap_data: List of dicts from the JSON beatmap.
+    :param tracks: List of Track objects.
+    :return: List of Note objects.
+    """
+    print(f"Generating notes with tracks: {tracks}")
+    notes = []
+    for entry in beatmap_data:
+        track_idx = entry["track"]
+        time = entry["time"]
 
-if __name__ == "__main__":
-    tracks = track.build_tracks()
-    note_x, note_y = build_notes(tracks)
-    print(note_x[0])
-    print(note_y[1])
-    print(note_x[i] for i in range(num_notes))
-    print(note_y[i] for i in range(num_notes))
+        # Validate track index
+        if track_idx < 0 or track_idx >= len(tracks):
+            print(f"Invalid track index: {track_idx}. Skipping note.")
+            continue
+
+        print(f"Track index: {track_idx}, Time: {time}")
+        notes.append(Note(track=tracks[track_idx], time=time))
+    return notes
+
